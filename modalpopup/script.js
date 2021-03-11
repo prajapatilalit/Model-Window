58f730dc2d39ef93205932dfb9f8323eede98043
prajapatilalit <prajapatilalit150992@gmail.com>
2021-03-11 18:56:28 +0530

    initial update

---
+++ modalpopup/script.js
@@ -0,0 +1,25 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('button click');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+  btnCloseModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
+
+  overlay.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
+}
